684b40032398beaef2ddf646430e5799b4df0b85
joe <joewalsh@vistulaofindiana.com>
2021-12-27 22:26:03 -0500

    add migration logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
 from app import routes
