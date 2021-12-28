23c823113c0f29247e6da5128d9a56d09b361462
joe <joewalsh@vistulaofindiana.com>
2021-12-27 21:16:47 -0500

    add models.py

---
+++ app/models.py
@@ -0,0 +1,18 @@
+
+from datetime import date, datetime
+from hashlib import md5
+from time import time
+from flask import current_app, request, url_for
+from app import db, login
+from sqlalchemy import or_, and_, extract, between
+
+
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True )
+    name = db.Column(db.String)
+    inorex = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        spacer = '**'
+        return f'{self.name:-^40} {spacer:>2} {self.inorex:^20}'
