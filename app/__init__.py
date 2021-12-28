8be57fa6c3e13b4796b4cb2c619f67dadf8920c6
joe <joewalsh@vistulaofindiana.com>
2021-12-27 22:32:19 -0500

    config migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
-db = SQLAlchemy()
-migrate = Migrate()
 
 from app import routes
