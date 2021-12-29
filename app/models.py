c05ab18d003bbe49bd746c5377af24a331e2485f
joe <joewalsh@vistulaofindiana.com>
2021-12-29 13:05:24 -0500

    add little db test

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from time import time
 from flask import current_app, request, url_for
 from app import db
 from sqlalchemy import or_, and_, extract, between
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Categories(db.Model):
@@ -16,3 +17,13 @@ class Categories(db.Model):
     def __repr__(self):
         spacer = '**'
         return f'{self.name:-^40} {spacer:>2} {self.inorex:^20}'
+
+
+cat = Categories()
+cat.name = 'test'
+db.session.add(cat)
+db.session.commit()
+
+result = Categories.query.all()
+for item in result:
+    print(item)
