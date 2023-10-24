8ab54931d96ab6fd8b27bc14158a230b73856399
joewalsh <joewalsh@vistulaofindiana.com>
2023-10-24 02:49:27 +0000

    add initial navbar

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,11 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index3.html', title='Big Bons!')
+    return render_template('coming_soon.html', title='Big Bons!')
 
-# @app.route('/marketing')
-# def marketing():
-#     return render_template('new_index.html')
+@app.route('/marketing')
+def marketing():
+    return render_template('new_index.html')
 
 
 @app.get('/home')
